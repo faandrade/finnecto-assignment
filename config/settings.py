e6e81981c1ec1805176060c50cffd340f33a8cf5
faandrade <faandrade@uc.cl>
2025-01-10 10:47:59 -0300

    add static root setting

--- config/settings.py
+++ config/settings.py
@@ -157,6 +157,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "dist",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
